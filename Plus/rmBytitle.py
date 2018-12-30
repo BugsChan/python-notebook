e8930cdc0ae907f63bfa48711f7ca7027e649c83
czw <3502318983@qq.com>
2018-12-30 15:47:39 +0800

    增加对title的滚动补全功能

--- Plus/rmBytitle.py
+++ Plus/rmBytitle.py
@@ -24,7 +24,7 @@ class Run:
                 del MyIO()._alltitles[arg1]
 
     @staticmethod
-    def complete(cmd, arg1, arg2):
+    def complete(cmd, arg1, arg2, loop=False):
         titlelist = []
         if cmd == "alltitles":
             return False
@@ -33,5 +33,30 @@ class Run:
             for each in titles:
                 if each.find(arg1) == 0:
                     titlelist.append(each)
-
+            if loop:
+                return list(titles.keys())
             return Listeners.getSame(titlelist)
+
+    @staticmethod
+    def next(cmd, arg1, arg2, prev=False):
+        titlelist = Run.complete(cmd, arg1, arg2, True)
+        if len(titlelist) == 0:
+            return False
+        titlelist.sort(reverse=prev)
+        if not arg1:
+            return titlelist[0]
+        else:
+            tmp = False
+            for each in titlelist:
+                if each == arg1:
+                    tmp = True
+                elif tmp:
+                    return each
+            for each in titlelist:
+                if each.find(arg1) == 0:
+                    return each
+            return False
+
+    @staticmethod
+    def last(cmd, arg1, arg2):
+        return Run.next(cmd, arg1, arg2, True)
--- Plus/title.py
+++ Plus/title.py
@@ -34,7 +34,7 @@ class Run:
             Listeners.putInput(grepByTitle(MyIO.getInstance(),arg1))
 
     @staticmethod
-    def complete(cmd,arg1,arg2):
+    def complete(cmd,arg1,arg2,loop=False):
         titlelist=[]
         if cmd=="alltitles":
             return False
@@ -43,6 +43,30 @@ class Run:
             for each in titles:
                 if each.find(arg1)==0:
                     titlelist.append(each)
-
+            if loop:
+                return list(titles.keys())
             return Listeners.getSame(titlelist)
 
+    @staticmethod
+    def next(cmd,arg1,arg2,prev=False):
+        titlelist=Run.complete(cmd,arg1,arg2,True)
+        if len(titlelist)==0:
+            return False
+        titlelist.sort(reverse=prev)
+        if not arg1:
+            return titlelist[0]
+        else:
+            tmp=False
+            for each in titlelist:
+                if each==arg1:
+                    tmp=True
+                elif tmp:
+                    return each
+            for each in titlelist:
+                if each.find(arg1)==0:
+                    return each
+            return False
+
+    @staticmethod
+    def last(cmd,arg1,arg2):
+        return Run.next(cmd,arg1,arg2,True)
