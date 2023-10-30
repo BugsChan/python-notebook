c60da8f550c0033cbb641799fc2bca0cd46c3f08
YandaChen <3502318983@qq.com>
2023-10-30 11:13:57 +0800

    增加了cmd的通用性

--- Plus/cmd.py
+++ Plus/cmd.py
@@ -2,23 +2,16 @@ import os
 from Listeners import Listeners
 
 class Run:
-    commands=["cmd","cmdinput"]
+    commands=["cmd"]
     introduction="""
-    cmd arg1 arg2:
-        将"arg1 arg2"作为字符串输入到命令行中
-    inputcmd :
-        将输入面板中的内容输入到命令行(shell)中
+    cmd arg1 arg2 ...:
+        将"arg1 arg2 ..."作为字符串输入到命令行中
     """
 
 
     @staticmethod
-    def run(cmd,arg1,arg2):
-        if cmd=="cmdinput":
-            myinput=Listeners.getInput()
-            os.system(myinput)
-        else:
-            if arg1==None:
-                arg1=""
-            if arg2==None:
-                arg2=""
-            os.system(arg1+" "+arg2)
+    def run(cmd,*args):
+        cmdstr = ""
+        for each in args:
+            cmdstr += each + " "
+        os.system(cmdstr)
